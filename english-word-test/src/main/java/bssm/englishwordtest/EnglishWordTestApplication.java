258c8593dd851cc53b283d1452de625a9b40b61d
Woowahan-Nyong <rltgjqmduftlagl@gmail.com>
2022-06-18 16:01:22 +0900

    SET :: (#01) Initialize Project

---
+++ english-word-test/src/main/java/bssm/englishwordtest/EnglishWordTestApplication.java
@@ -0,0 +1,13 @@
+package bssm.englishwordtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishWordTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishWordTestApplication.class, args);
+	}
+
+}
---
+++ english-word-test/src/test/java/bssm/englishwordtest/EnglishWordTestApplicationTests.java
@@ -0,0 +1,13 @@
+package bssm.englishwordtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishWordTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
